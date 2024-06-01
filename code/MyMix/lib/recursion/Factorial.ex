15f7b503fa681f11dd9930ac099b75f21eecc078
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-06-01 13:05:23 +0300

    Lixering

--- code/MyMix/lib/recursion/Factorial.ex
+++ code/MyMix/lib/recursion/Factorial.ex
@@ -3,5 +3,9 @@ defmodule MyMix.Recusrsion.Factorial do
   def factorial(1), do: 1
   def factorial(num), do: num * factorial(num -1)
 
+  def rec_factorial(num, acc \\ 1)
+  def rec_factroial(1, acc), do: acc
+  def rec_factorial(num, acc), do: rec_factorial(num - 1, acc * num)
+
 
 end
